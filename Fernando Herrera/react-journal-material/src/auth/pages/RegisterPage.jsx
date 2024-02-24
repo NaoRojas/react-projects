9b1b149a7bad99836ed1f3fd0c40a048ae0c3a38
naoreds <naorojash@gmail.com>
2024-02-24 15:57:24 -0600

    Refactor RegisterPage and useForm hooks

--- Fernando Herrera/react-journal-material/src/auth/pages/RegisterPage.jsx
+++ Fernando Herrera/react-journal-material/src/auth/pages/RegisterPage.jsx
@@ -1,63 +1,97 @@
-import { Link as RouterLink } from 'react-router-dom';
-import { Button, Grid, Link, TextField, Typography } from '@mui/material';
-import { Google } from '@mui/icons-material';
-import { AuthLayout } from '../layout/AuthLayout';
+import { Link as RouterLink } from 'react-router-dom'
+import { Button, Grid, Link, TextField, Typography } from '@mui/material'
+import { AuthLayout } from '../layout/AuthLayout'
+import { useForm } from '../../hooks'
 
+const formData = {
+  password: 'nao@admin.com',
+  email: 'nao@admin.com',
+  displayName: 'nao',
+}
+const formValidations = {
+  email: [(value) => value.includes('@'), 'Correo no válido'],
+  displayName: [(value) => value.trim().length > 0, 'Nombre no válido'],
+  password: [(value) => value.length > 5, 'Contraseña no válida'],
+}
 
 export const RegisterPage = () => {
+  const {
+    displayName,
+    email,
+    password,
+    onInputChange,
+    formState,
+    isFormValid,
+    emailValid,
+    displayNameValid,
+    passwordValid,
+  } = useForm(formData, formValidations)
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(formState)
+  }
   return (
     <AuthLayout title="Crear cuenta">
-      <form>
-          <Grid container>
-           
-            <Grid item xs={ 12 } sx={{ mt: 2 }}>
-              <TextField 
-                label="Nombre completo" 
-                type="text" 
-                placeholder='Nombre completo' 
-                fullWidth
-              />
-            </Grid>
-
-            <Grid item xs={ 12 } sx={{ mt: 2 }}>
-              <TextField 
-                label="Correo" 
-                type="email" 
-                placeholder='correo@google.com' 
-                fullWidth
-              />
-            </Grid>
-
-            <Grid item xs={ 12 } sx={{ mt: 2 }}>
-              <TextField 
-                label="Contraseña" 
-                type="password" 
-                placeholder='Contraseña' 
-                fullWidth
-              />
-            </Grid>
-            
-            <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
-              <Grid item xs={ 12 }>
-                <Button variant='contained' fullWidth>
-                  Crear cuenta
-                </Button>
-              </Grid>
-            </Grid>
-
-
-            <Grid container direction='row' justifyContent='end'>
-              <Typography sx={{ mr: 1 }}>¿Ya tienes cuenta?</Typography>
-              <Link component={ RouterLink } color='inherit' to="/auth/login">
-                ingresar
-              </Link>
-            </Grid>
-
+      <form onSubmit={onSubmit}>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              fullWidth
+              label="Nombre completo"
+              type="text"
+              name="displayName"
+              value={displayName}
+              onChange={onInputChange}
+              error={displayNameValid}
+              helperText={displayNameValid}
+              placeholder="Nombre completo"
+            />
           </Grid>
 
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Correo"
+              type="email"
+              name="email"
+              value={email}
+              onChange={onInputChange}
+              placeholder="correo@google.com"
+              fullWidth
+              error={emailValid}
+              helperText={emailValid}
+            />
+          </Grid>
 
-        </form>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Contraseña"
+              type="password"
+              name="password"
+              value={password}
+              onChange={onInputChange}
+              placeholder="Contraseña"
+              error={passwordValid}
+              helperText={passwordValid}
+              fullWidth
+            />
+          </Grid>
 
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12}>
+              <Button variant="contained" fullWidth type="submit">
+                Crear cuenta
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" justifyContent="end">
+            <Typography sx={{ mr: 1 }}>¿Ya tienes cuenta?</Typography>
+            <Link component={RouterLink} color="inherit" to="/auth/login">
+              ingresar
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
     </AuthLayout>
   )
 }
--- Fernando Herrera/react-journal-material/src/hooks/useForm.js
+++ Fernando Herrera/react-journal-material/src/hooks/useForm.js
@@ -1,7 +1,12 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
-export const useForm = (initialForm = {}) => {
+export const useForm = (initialForm = {}, fomrValidations = {}) => {
   const [formState, setFormState] = useState(initialForm)
+  const [formValidation, setFormValidation] = useState({})
+
+  useEffect(() => {
+    createValidations()
+  }, [formState])
 
   const onInputChange = ({ target }) => {
     const { name, value } = target
@@ -15,10 +20,21 @@ export const useForm = (initialForm = {}) => {
     setFormState(initialForm)
   }
 
+  const createValidations = () => {
+    const formCheckedValues = {}
+    for (const formField of Object.keys(fomrValidations)) {
+      const [fn, erroMessage = 'Este campo es requerido'] = fomrValidations[formField]
+      formCheckedValues[`${formField}Valid`] = fn(formState[formField]) ? null : erroMessage
+    }
+    setFormValidation(formCheckedValues)
+  }
+
+
   return {
     ...formState,
     formState,
     onInputChange,
     onResetForm,
+    ...formValidation
   }
 }
